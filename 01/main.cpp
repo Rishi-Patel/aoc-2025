32175afa8fd82e9ca896cfbb4b4371883379bcdb
Rishi-Patel <patelris@umich.edu>
2025-12-01 00:58:47 -0500

    Finish day01

--- 01/main.cpp
+++ 01/main.cpp
@@ -2,15 +2,64 @@
 #include <iostream>
 #include "utils.h"
 #include <string>
+#include <cassert>
+
+void Part1() {
+    std::string line;
+    int num = 50;
+    int ans = 0;
+    while (std::getline(std::cin, line)) { 
+        if (line[0]  == 'L') {
+            num -= std::stoi(line.substr(1));
+        }
+        else if (line[0] == 'R') {
+            num += std::stoi(line.substr(1));
+        }
+        if (num < 0) {
+            num += 100;
+        }
+        num = num % 100;
+        if (num == 0) {
+            ans++;
+        }
+    }
+    std::cout << ans << std::endl;
+}
+
+void Part2() {
+    std::string line;
+    int num = 50;
+    int ans = 0;
+    while (std::getline(std::cin, line)) { 
+        int old = num;
+        if (line[0]  == 'L') {
+            num -= std::stoi(line.substr(1));
+        }
+        else if (line[0] == 'R') {
+            num += std::stoi(line.substr(1));
+        }
+        // Account for case where we land on 0
+        if (num == 0) {
+            ans++;
+        }
+        // Account for first left to right or right to left
+        if (num * old < 0) {
+            ans++;
+        }
+        // Accoutnf for full rotations
+        ans += std::abs(num / 100);
+
+        // Move num back to 0-99
+        num = num % 100;
+        if (num < 0) {
+            num += 100;
+        }
+    }
+    std::cout << ans << std::endl;
+}
 
 int main(int argc, char* argv[]) {
-    std::string line;
-    while (std::getline(std::cin, line)) { 
-        auto parts = Split(line, " ");
-        for (const auto& part : parts) {
-            std::cout << part << std::endl;
-        }
-        std::cout << "--------------------------------" << std::endl;
-    }
+    // Part1();
+    Part2();
     return 0;
 }
